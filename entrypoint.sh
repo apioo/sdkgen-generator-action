220b0c7d2cd3c4749dcb28dfc7b697fb00967730
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-02-27 19:39:09 +0100

    create tag and release version if provided and use fitting commit message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,34 @@
 #!/bin/sh -l
+git config --global user.name "SDKgen-Bot";
+git config --global user.email "bot@sdkgen.app";
+git config --global --add safe.directory /github/workspace
+
+if [ ! -z "$TYPEHUB_VERSION" ]
+then
+  echo "Creating tag $TYPEHUB_VERSION ..."
+  git tag -a "v$TYPEHUB_VERSION" -m "$TYPEHUB_CHANGELOG"
+  git push https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git "v$TYPEHUB_VERSION"
+
+  gh release create "v$TYPEHUB_VERSION" -n "$TYPEHUB_CHANGELOG"
+  exit
+fi
+
 git symbolic-ref -q HEAD
 if [ $? -eq 1 ]
 then
   echo "Git is in detached HEAD mode skip ..."
   exit
 fi
+
+echo "Generate code  ..."
+
 /usr/local/bin/sdkgen update --remove
-git config --global user.name "SDKgen-Bot";
-git config --global user.email "bot@sdkgen.app";
-git config --global --add safe.directory /github/workspace
+
+if [ -z "$TYPEHUB_MESSAGE" ]
+then
+  TYPEHUB_MESSAGE="Update code generation"
+fi
+
 git add -A
-git commit -m "Update code generation"
+git commit -m "$TYPEHUB_MESSAGE"
 git push https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
